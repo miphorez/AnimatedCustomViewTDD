1145ee0a452d63f1b1a4c8d7ac379ae5972e0a91
miphorez <miphorez@gmail.com>
2020-12-07 09:05:54 +0300

    added ui test

---
+++ app/src/androidTest/java/com/example/customview/MainActivityTest.java
@@ -0,0 +1,32 @@
+package com.example.customview;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void onCreate() {
+        onView(withId(R.id.textView))
+                .check(matches(withText("Hello")));
+    }
+}
